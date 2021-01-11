82e72a5228be4d05026cc422c7b874058401fa93
sharadshivale <58933575+sharadshivale@users.noreply.github.com>
2021-01-11 23:05:55 +0530

    DISTRO-4464:testcommit

---
+++ test-sql.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+
+--changeset schauhan:1
+
+select * from releases;
+selcet * from release_approva;;
+
+--rollback select * from releases;
+--rollback select * from project;
